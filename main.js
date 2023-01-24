be816fd8bbd4340de42e0410d9ec39025520c9c2
Leandroluna12 <leandrogaston1052@gmail.com>
2023-01-24 15:33:39 -0300

    se le hicieron modificaciones al trabajo, lo que se le hizo fue agregar arrays y objetos, se hicieron mas funciones y ahora se puede ver el precio total y porcentaje de la compra

--- main.js
+++ main.js
@@ -5,38 +5,133 @@ function saludar() {
 saludar();
 
 
-let productos = Number(prompt("Ingrese que producto necesitas comprar 1-remera 2-buzo 3-camiseta"));
+
+let productos = Number(prompt("Ingrese que producto necesitas comprar \n1-agarre perro \n2-buzo perro \n3-chaleco gato \n4-chaleco perro \n5-remera Argentina \n6-bandana perro \n7-campera disney \n8-vestido calabaza perro"));
+
+
+
+const productosShop = [
+    {
+        id: 1,
+        picture: "./imagenes/agarrePerro.webp",
+        nombre: "agarre perro",
+        text: "Agarre para perro, estatura chica o mediana.",
+        precio: 1200
+    },
+
+    {
+        id: 2,
+        picture: "./imagenes/buzoPerro.webp",
+        nombre: "buzo perro",
+        text: "Buzo para perro, estatura chica o grande",
+        precio: 1500
+    },
+
+    {
+        id: 3,
+        picture: "./imagenes/chalecoGato.webp",
+        nombre: "chaleco gato",
+        text: "Este es un chaleco para gato, solo para estatura chica o mediana",
+        precio: 1000
+    },
+
+    {
+        id: 4,
+        picture: "./imagenes/chalecoPerro.webp",
+        nombre: "chaleco perro",
+        text: "chaleco para perro exclusivo, solo para estatura chica o mediana",
+        precio: 1000
+    },
+
+    {
+        id: 5,
+        picture: "./imagenes/remeraArgentinaPerro.webp",
+        nombre: "remera argentina",
+        text: "Remera Argentina exclusiva, solo para estatura grande",
+        precio: 1600
+    },
+
+    {
+        id: 6,
+        picture: "./imagenes/bandanaPerro.jpg",
+        nombre: "bandana perro",
+        text: "Bandana para perro exclusiva, para todas las estaturas",
+        precio: 800
+    },
+
+    {
+        id: 7,
+        picture: "./imagenes/camperaDisney.webp",
+        nombre: "campera disney",
+        text: "campera disney, solo para talles chicos",
+        precio: 1200
+    },
+
+    {
+        id: 8,
+        picture: "./imagenes/vestidoCalabazaPerro.webp",
+        nombre: "vestido calabaza perro",
+        text: "vestido calabaza para perros, solo para estatura grande",
+        precio: 1700
+    },
+]
+
+
+function seleccionProductos(productosArray,prod) {
+    let resultado = "";
+    productosArray.forEach(function (elemento) {
+
+        if (elemento.id == prod) {
+            resultado = elemento;
+        }
+    });
+    return resultado;
+}
+
+console.log(seleccionProductos(productosShop,productos));
+
+alert("nombre: " + seleccionProductos(productosShop,productos).nombre + ", Precio: " + seleccionProductos(productosShop,productos).precio);
+
+
 
 let cantidadProductos = Number(prompt("Ingrese cuantos productos necesitas. le seleccion debe ser maximo de 20 productos. "))
 
+function calcularDescuento(cantidad) {
 
-switch (productos) {
-    case 1: alert("Usted selecciono remera " + "con una cantidad de: " + cantidadProductos + " productos.")
-        break;
-    case 2: alert("Usted selecciono buzo " + "con una cantidad de: " + cantidadProductos + " productos.")
-        break;
-    case 3: alert("Usted selecciono camiseta " + "con una cantidad de: " + cantidadProductos + " productos.")
-        break;
+    let descuento = 0;
+    if (cantidad >= 15) {
 
-    default:
-        alert("No has seleccionado ningun producto.")
-        break;
+        descuento = 20;
+    } else if (cantidad >= 6 && cantidad < 15) {
+        descuento = 10;
+
+    } 
+
+    return descuento;
 }
 
-function calcularDescuento(cantidadProductos) {
+alert("usted va a recibir el " + calcularDescuento(cantidadProductos) + "% de descuento")
 
-    if (cantidadProductos >= 15) {
-
-        alert("usted va a recibir el 20% descuento")
-    } else if (cantidadProductos>=6 && cantidadProductos<15) {
-        alert("usted va a recibir el 10% de descuento")
-
-    } else {
-        alert("usted no recibe descuento.")
-
-    }
+function calcularTotal(productos, cantidad, productosShop) {
+    let descuento = calcularDescuento(cantidad);
+    let productoElegido = seleccionProductos(productosShop,productos)
+    let totalParcial = productoElegido.precio*cantidad;
+    let total = totalParcial * (100-descuento)/100;
+    return total;
 }
-calcularDescuento(cantidadProductos);
+
+alert("usted debe abonar $" + calcularTotal(productos,cantidadProductos, productosShop));
+
+// const productosShop2 = [{
+//     id: 9,
+//     picture: "./imagenes/vestidoCalabazaPerro.webp",
+//     nombre: "casco calabaza perro",
+//     text: "casco calabaza para perros, solo para estatura grande",
+//     precio: "$1700"
+// }]
 
 
+// const productosFinal = productosShop.concat(productosShop2);
+
+// console.log(productosFinal)
 
